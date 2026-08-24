3cd59aa017abd66e0745918cc7e5d0f20da8e13e
mdszhossain <personalmailshazzad@gmail.com>
2026-08-24 13:42:00 +0600

    success page creation done

--- controller/reviewFormController.js
+++ controller/reviewFormController.js
@@ -1,3 +1,6 @@
 module.exports.renderReviewForm = async(req, res) => {
     res.render("reviewForm.ejs");
 }
+module.exports.renderSuccessPage = async(req, res) => {
+    res.render("successPage.ejs");
+}
--- routes/reviewerRouter.js
+++ routes/reviewerRouter.js
@@ -4,5 +4,6 @@ const reviewFormController = require("../controller/reviewFormController");
 const wrapAsync = require("../utils/wrapAsync");
 
 router.get("/reviewForm", wrapAsync(reviewFormController.renderReviewForm));
+router.get("/reviewForm/success", wrapAsync(reviewFormController.renderSuccessPage));
 
 module.exports = router;
