3d8461800084a051c7a4e08652b7ec68a48fe681
mdszhossain <personalmailshazzad@gmail.com>
2026-08-27 01:08:16 +0600

    signup success page created for client side

--- controller/userController.js
+++ controller/userController.js
@@ -4,12 +4,13 @@ module.exports.renderSignupPage = async (req, res) => {
   res.render("signup.ejs");
 };
 module.exports.renderSigninPage = async (req, res) => {
-  res.render("signin.ejs");
+    res.render("signin.ejs");
+//   res.render("signupSuccess.ejs");
 };
 module.exports.registerUser = async (req, res) => {
   const { fullName, username, email, phone, password } = req.body;
   console.log(req.body);
   const newUser = new User({ fullName, username, email, phone });
   const registeredUser = await User.register(newUser, password);
-  res.send("user signup done");
+  res.render("signupSuccess.ejs");
 };
