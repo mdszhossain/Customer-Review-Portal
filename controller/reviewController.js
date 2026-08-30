4e3be9a13005f2f63b045819d4b448b947712d6e
mdszhossain <personalmailshazzad@gmail.com>
2026-08-30 20:44:27 +0600

    review page done

--- controller/reviewController.js
+++ controller/reviewController.js
@@ -1,6 +1,26 @@
 const Shop = require("../models/shopModel");
+const Review = require("../models/reviewModel");
 module.exports.renderShopReviewPage = async (req, res) => {
   const { id } = req.params;
   const shop = await Shop.findOne({ _id: id });
-  res.render("reviewPage.ejs", { shop });
+  const reviews = await Review.find({ shopId: id });
+  let totalReviews = reviews.length;
+  let totalRating = 0;
+  for (review of reviews) {
+    totalRating += review.star;
+  }
+  let averageRating = totalRating / totalReviews;
+  let reviews4 = await Review.find({ shopId: id, star: 4 });
+  let reviews5 = await Review.find({ shopId: id, star: 5 });
+  let reviews1To3 = await Review.find({ shopId: id, star: { $gte: 1, $lte: 3 } });
+  console.log(reviews1To3);
+  res.render("reviewPage.ejs", {
+    shop,
+    reviews,
+    totalReviews,
+    averageRating,
+    reviews1To3,
+    reviews4,
+    reviews5,
+  });
 };
--- controller/reviewFormController.js
+++ controller/reviewFormController.js
@@ -1,21 +1,24 @@
 const Review = require("../models/reviewModel");
+const Shop = require("../models/shopModel");
 const QRCode = require("qrcode");
 
 module.exports.renderReviewForm = async (req, res) => {
-  console.log(req.originalUrl);
-  res.render("reviewForm.ejs");
+  const {id} = req.params;
+  res.render("reviewForm.ejs", {id});
 };
 module.exports.submitReview = async (req, res) => {
   const { star, message, customerName, phone } = req.body;
-  const newReview = new Review({ star, message, customerName, phone });
+  const { id } = req.params;
+  const shop = await Shop.findOne({ _id: id });
+  const newReview = new Review({ star, message, customerName, phone, shopId: shop });
   await newReview.save();
   // res.redirect("/crp/reviewForm/success");
-  res.render("successPage.ejs", { star });
+  res.render("successPage.ejs", { star, shop });
 };
 
 module.exports.qrCodeGen = async (req, res) => {
-    const {id} = req.params;
-    const reviewUrl = `${req.protocol}://${req.get("host")}/crp/${id}/reviewForm`;
-    const qrDataUrl = await QRCode.toDataURL(reviewUrl);
-    res.render("qrcode.ejs", {qrDataUrl, reviewUrl});
+  const { id } = req.params;
+  const reviewUrl = `${req.protocol}://${req.get("host")}/crp/${id}/reviewForm`;
+  const qrDataUrl = await QRCode.toDataURL(reviewUrl);
+  res.render("qrcode.ejs", { qrDataUrl, reviewUrl });
 };
